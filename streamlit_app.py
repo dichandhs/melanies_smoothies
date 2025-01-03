744551f0c207f0d67e6b77395c708795c4d8672d
dichandhs <50727870+dichandhs@users.noreply.github.com>
2025-01-03 12:35:12 +0530

    Update streamlit_app.py
    Typo correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #Convert the Snowpark dataframe to a pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingradient_list = st.multiselect('Choose upto 5 ingradients:',my_dataframe,max_selections=5)
 
